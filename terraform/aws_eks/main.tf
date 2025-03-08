030e406643da386904b64cf3ee60649350c0dd2a
Mk <your-email@example.com>
2025-03-08 19:38:27 +0530

    up

--- terraform/aws_eks/main.tf
+++ terraform/aws_eks/main.tf
@@ -84,7 +84,7 @@ resource "aws_eks_cluster" "eks_cluster" {
 }
 
 resource "aws_eks_node_group" "eks_nodes" {
-  cluster_name  = aws_eks_cluster.eks.name
+  cluster_name  = aws_eks_cluster.eks_cluster.name
   node_role_arn = aws_iam_role.worker_nodes.arn
   subnet_ids    = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id] # Nodes distributed in these subnets
 
