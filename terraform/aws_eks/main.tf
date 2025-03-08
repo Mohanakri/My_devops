9f92eae45a4f80021b99e831d9b30c3d6ae0b8ec
Mk <your-email@example.com>
2025-03-08 21:20:04 +0530

    fmt

--- terraform/aws_eks/main.tf
+++ terraform/aws_eks/main.tf
@@ -219,7 +219,7 @@ resource "aws_eks_node_group" "eks_workers" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = "eks-node-group"
   node_role_arn   = aws_iam_role.eks_worker_role.arn
-  subnet_ids      = [
+  subnet_ids = [
     aws_subnet.eks_private_subnet_a.id,
     aws_subnet.eks_private_subnet_b.id
   ]
