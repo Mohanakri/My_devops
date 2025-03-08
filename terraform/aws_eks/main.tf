708681b188b874a870fa35adac8adb150b14dbe6
Mk <your-email@example.com>
2025-03-08 19:32:12 +0530

    working nodes

--- terraform/aws_eks/main.tf
+++ terraform/aws_eks/main.tf
@@ -83,6 +83,18 @@ resource "aws_eks_cluster" "eks_cluster" {
   }
 }
 
+resource "aws_eks_node_group" "eks_nodes" {
+  cluster_name  = aws_eks_cluster.eks.name
+  node_role_arn = aws_iam_role.worker_nodes.arn
+  subnet_ids    = [aws_subnet.public1.id, aws_subnet.public2.id] # Nodes distributed in these subnets
+
+  scaling_config {
+    desired_size = 2 # 2 worker nodes
+    max_size     = 3
+    min_size     = 1
+  }
+}
+
 
 
 
